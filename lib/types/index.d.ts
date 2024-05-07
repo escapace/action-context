5cdaea6b89de51a8a8e1eef14fe6fb5e9a132e83
yyxi <178086254+yyxi@users.noreply.github.com>
2024-05-07 09:11:39 +0000

    chore: does this work with esm?

---
+++ lib/types/index.d.ts
@@ -0,0 +1,4 @@
+export declare const getBranch: () => string;
+export declare const assertRepoNotShallow: () => Promise<void>;
+export declare function getLastGitTag(branch?: string): Promise<string | undefined>;
+//# sourceMappingURL=index.d.ts.map
