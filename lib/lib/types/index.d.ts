b67d9331790418ef7f80e757ddf4aa8b95fdb17c
yyxi <178086254+yyxi@users.noreply.github.com>
2024-05-09 12:20:00 +0000

    chore: gitignore

--- lib/lib/types/index.d.ts
+++
@@ -1,4 +0,0 @@
-export declare const getBranch: () => string;
-export declare const assertRepoNotShallow: () => Promise<void>;
-export declare function getLastGitTag(branch?: string): Promise<string | undefined>;
-//# sourceMappingURL=index.d.ts.map
