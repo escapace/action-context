c6c6c9b3a9d9f3e328236827d4d377d5e44207e5
yyxi <178086254+yyxi@users.noreply.github.com>
2024-07-16 05:41:22 +0000

    fix: context

--- lib/action/node_modules/.pnpm/@pnpm+matcher@6.0.0/node_modules/@pnpm/matcher/lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-type Matcher = (input: string) => boolean;
-type MatcherWithIndex = (input: string) => number;
-export declare function createMatcher(patterns: string[] | string): Matcher;
-export declare function createMatcherWithIndex(patterns: string[]): MatcherWithIndex;
-export {};
